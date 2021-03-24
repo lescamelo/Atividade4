3897374bd1433b5c30505015ba698de5168b6ef1
Valesca Melo <valesca.melo@academico.ifpb.edu.br>
2021-03-23 21:24:49 -0300

    .

---
+++ quest3.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+read -p "Informe o primeiro arquivo: " arquivo1
+read -p "Informe o segundo arquivo: " arquivo2
+
+a1=$(wc -l ${arquivo1} | cut -c 1)
+
+a2=$(wc -l ${arquivo2} | cut -c 1)
+
+(( ${a1} > ${a2} ))  &&  echo "Arquivo que contém mais linhas: ${arquivo1}" || echo "Arquivo que contém mais linhas ${arquivo2}"
